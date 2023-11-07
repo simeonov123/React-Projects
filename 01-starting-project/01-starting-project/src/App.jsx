4cf95ab0c5457f0711dbd21cb17f7b8a04039e34
Simeon <simeonov230@gmail.com>
2023-11-07 13:09:01 +0200

    Added componenets and lifted use state.

--- 01-starting-project/01-starting-project/src/App.jsx
+++ 01-starting-project/01-starting-project/src/App.jsx
@@ -1,6 +1,34 @@
+import Header from "./component/Header.jsx";
+import Results from "./component/Results.jsx";
+import UserInput from "./component/UserInput.jsx";
+import { useState } from "react";
+
 function App() {
+  const [userInput, setUserInput] = useState(
+    {
+        initialInvestment: 1000,
+        anualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10
+    }
+);
+
+
+function handleChange(inputIdentifier, newValue){
+  setUserInput(previousUserInput =>{
+      return{
+          ...previousUserInput,
+          [inputIdentifier] : newValue
+      };
+  });
+}
+
   return (
-    <h1>React Investment Calculator</h1>
+    <>
+      <Header />
+      <UserInput  userInput={userInput} onChange ={handleChange}/>
+      <Results input = {userInput}/>
+    </>
   )
 }
 
