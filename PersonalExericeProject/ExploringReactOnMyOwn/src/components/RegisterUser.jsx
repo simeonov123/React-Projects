3d552a2f70c68d8aa249b4ef2fde448960c9f46a
Simeon <simeonov230@gmail.com>
2023-11-10 03:04:23 +0200

    Task Manager Project

---
+++ PersonalExericeProject/ExploringReactOnMyOwn/src/components/RegisterUser.jsx
@@ -0,0 +1,14 @@
+
+
+export default function Register(){
+
+
+  return(
+
+    <div className = "bg-slate-500 w-full h-screen">
+      <h1 className = "text-4xl text-center pt-8 flex items-center justify-center">My attempt</h1>
+    </div>
+
+  );
+
+}
