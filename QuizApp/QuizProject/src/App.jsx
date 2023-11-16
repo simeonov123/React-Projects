ed743474dc7e2f884a052c1a2ae7d9a18bf13da8
Simeon <simeonov230@gmail.com>
2023-11-16 17:11:48 +0200

    Finished the Quiz app

--- QuizApp/QuizProject/src/App.jsx
+++ QuizApp/QuizProject/src/App.jsx
@@ -1,5 +1,5 @@
-import Header from "./components/Header";
-import Quiz from "./components/Quiz";
+import Header from './components/Header.jsx';
+import Quiz from './components/Quiz.jsx';
 
 function App() {
   return (
--- QuizApp/QuizProject/src/components/Header.jsx
+++ QuizApp/QuizProject/src/components/Header.jsx
@@ -1,10 +1,10 @@
-import logoImg from "../assets/quiz-logo.png";
+import logoImg from '../assets/quiz-logo.png';
 
 export default function Header() {
   return (
     <header>
       <img src={logoImg} alt="Quiz logo" />
-      <h1>React quiz</h1>
+      <h1>ReactQuiz</h1>
     </header>
   );
 }
